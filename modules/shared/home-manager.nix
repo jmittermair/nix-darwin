5cb09c274ba13b8335387f88c26eadc0edef8446
James Mittermair <48706313+jmittermair@users.noreply.github.com>
2025-05-01 09:38:00 +1000

    removing p10k and adding starship to zshrc

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -9,23 +9,14 @@ let name = "James Mittermair";
     enable = true;
     autocd = false;
     cdpath = [ "~/.local/share/src" ];
-    plugins = [
-      {
-          name = "powerlevel10k";
-          src = pkgs.zsh-powerlevel10k;
-          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      }
-      {
-          name = "powerlevel10k-config";
-          src = lib.cleanSource ./config;
-          file = "p10k.zsh";
-      }
-    ];
     initExtraFirst = ''
       if [[ -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]]; then
         . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
         . /nix/var/nix/profiles/default/etc/profile.d/nix.sh
       fi
+      
+      # Begin starship
+      eval "$(starship init zsh)"
 
       # Define variables for directories
       export PATH=$HOME/.local/share/bin:$PATH
